4db6a8a33b4485386c8364797c74da03f661a829
Rachel Burke <31460482+RachelBurke@users.noreply.github.com>
2019-08-08 23:21:48 -0400

    Adding Assignments 17, 18, & MyCalculator Project

---
+++ Assignment 18 - Rachel Burke/StringsUnitTestTutorial/StringsUnitTestTutorial/StringTesting/SpecialValueTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using StringsUnitTestTutorial;
+
+//to run these tests you need to Build > Build Solution
+
+namespace StringTesting
+{
+    
+    [TestClass]
+    public class SpecialValueTests
+    {
+        strManipulator objManipulator;
+
+        [TestMethod]
+        public void TestStringSearch()
+        {
+            objManipulator = new strManipulator("aaabaaabaaa");
+
+            Assert.IsTrue(objManipulator.search("a"), "this message is displayed when the assert fails");
+            Assert.IsTrue(objManipulator.search("b"), "b is not found in the string");
+            Assert.IsTrue(objManipulator.search("aa"), "aa is not found in the string");
+            Assert.IsTrue(objManipulator.search("aba"), "aba is not found in the string");
+            Assert.IsTrue(objManipulator.search("aaa"), "aaa is not found in the string");
+            Assert.IsTrue(objManipulator.search(""), "empty string is not found");
+
+            Assert.IsFalse(objManipulator.search("bbb"), "bbb is part of the string");
+            Assert.IsFalse(objManipulator.search("xy"), "xy is part of the string");
+            Assert.IsFalse(objManipulator.search(" "), "white space is part of the string");
+            Assert.IsFalse(objManipulator.search("bb"), "bb is part of the string");
+        }
+        [TestMethod]
+        public void TestStringStripper()
+        {
+            objManipulator = new strManipulator("aaabaaabaaa");
+
+            Assert.AreEqual(objManipulator.stripper("a", ""), "bb");
+            Assert.AreEqual(objManipulator.stripper("a", "x"), "xxxbxxxbxxx");
+            Assert.AreEqual(objManipulator.stripper("b", "W"), "aaaWaaaWaaa");
+            Assert.AreEqual(objManipulator.stripper("aaabaaabaaa", ""), "");
+
+            Assert.AreNotEqual(objManipulator.stripper("b", ""), "aaaaaaaa");
+            Assert.AreNotEqual(objManipulator.stripper("a", "x"), "aaaWaaaWaaa");
+
+        }
+        [TestMethod]
+        public void TestStringWrapper()
+        {
+            objManipulator = new strManipulator("aaabaaabaaa");
+
+            Assert.AreEqual(objManipulator.wrapper("XXX", "YYY"), "XXXaaabaaabaaaYYY");
+            Assert.AreNotEqual(objManipulator.wrapper("XXX", "YYY"), "XaaabaaabaaaYYY");
+
+            StringAssert.StartsWith(objManipulator.wrapper("AAA", "WWW"), "AAA");
+            StringAssert.EndsWith(objManipulator.wrapper("XXX", "YYY"), "YYY");
+            StringAssert.Contains(objManipulator.wrapper("XXX", "YYY"), "XaaabaaabaaaY");
+
+
+        }
+        [TestMethod]
+        public void TestStringCapializer()
+        {
+            objManipulator = new strManipulator("aaabaaabaaa");
+
+            Assert.AreEqual(objManipulator.capitalizer("a"), "A");
+            Assert.AreEqual(objManipulator.capitalizer("b"), "B");
+            Assert.AreEqual(objManipulator.capitalizer("aa"), "AA");
+            Assert.AreEqual(objManipulator.capitalizer("aba"), "ABA");
+            Assert.AreEqual(objManipulator.capitalizer("aaa"), "AAA");
+            Assert.AreEqual(objManipulator.capitalizer(""), "");
+            Assert.AreEqual(objManipulator.capitalizer("aAa"), "AAA");
+            Assert.AreEqual(objManipulator.capitalizer("aaa bbb"), "AAA BBB");
+            Assert.AreEqual(objManipulator.capitalizer("aaabaaa"), "AAABAAA");
+
+        }
+    }
+}
---
+++ MyCalculator/MyCalculator/Calculator.cs
@@ -0,0 +1,30 @@
+﻿//removed redundancies caused by using in Code Issues
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace MyCalculator
+{
+    public class Calculator
+    {
+        //Clicked on Generate Code and added this constructor using ReSharpner
+        public Calculator() //Can remove per code inspection
+        {
+            return;
+        }
+
+        //Ran code cleanup - nothing to be cleaned
+
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+
+        public int Divide(int dividend, int divisor)
+        {
+            return dividend / divisor;
+        }
+    }
+}
